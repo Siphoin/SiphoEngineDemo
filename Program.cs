8877a73b70b8fe2abf665dd0dc3a67909a8b5acc
Maxim <36982547+Siphoin@users.noreply.github.com>
2025-05-18 11:32:48 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
-﻿using System.Diagnostics;
+﻿using SFML.System;
 using SiphoEngine;
 using SiphoEngine.Components;
 using SiphoEngine.Core;
+using SiphoEngine.Core.Components.Render;
 using SiphoEngine.Core.SiphoEngine;
 
 namespace SiphoEngineDemo
@@ -10,14 +11,30 @@ namespace SiphoEngineDemo
     {
         static void Main(string[] args)
         {
+
             Game game = new();
+            game.OnRunning += Test;
+            game.Run();
+
+        }
+
+        private static void Test()
+        {
             TestScene testScene = new TestScene();
-            GameEngine.AddScene(testScene);
-            GameObject gameObject = testScene.CreateGameObject();
+
+            // Создаем камеру
+            var cameraObj = testScene.CreateGameObject("MainCamera");
+            var camera = cameraObj.AddComponent<Camera>();
+            camera.OrthographicSize = 250f; // Видимая область: 10 единиц по высоте
+            cameraObj.Transform.Position = new Vector2f(0, 0);
+
+            // Создаем тестовый объект
+            var gameObject = testScene.CreateGameObject("TestObject");
             gameObject.AddComponent<SpriteRenderer>();
             gameObject.AddComponent<PlayerController>();
-            gameObject.Transform.Position = new(150, 150);
-            game.Run();
+            gameObject.Transform.Position = new Vector2f(0, 0); // В центре камеры
+
+            GameEngine.AddScene(testScene);
         }
     }
 
