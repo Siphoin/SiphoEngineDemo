893a41ebf2e4223a7d2db7de727161b34479b4b1
Maxim <36982547+Siphoin@users.noreply.github.com>
2025-05-18 11:10:36 +0500

    first demo version

---
+++ PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using SFML.System;
+using SiphoEngine.Core;
+using SiphoEngine.Core.PlayerLoop;
+using SiphoEngine.MathExtensions;
+using Time = SiphoEngine.Core.Time;
+
+namespace SiphoEngineDemo
+{
+    public class PlayerController : Component, IUpdatable
+    {
+        public float Speed = 500f;
+
+        public void Update()
+        {
+            Vector2f direction = new Vector2f();
+
+            if (Input.GetKey(SFML.Window.Keyboard.Key.W)) direction.Y -= 1;
+            if (Input.GetKey(SFML.Window.Keyboard.Key.S)) direction.Y += 1;
+            if (Input.GetKey(SFML.Window.Keyboard.Key.A)) direction.X -= 1;
+            if (Input.GetKey(SFML.Window.Keyboard.Key.D)) direction.X += 1;
+
+            if (!direction.IsZero())
+                Transform.Position += direction.Normalized() * Speed * Time.DeltaTime;
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,32 @@
-﻿namespace SiphoEngineDemo
+﻿using System.Diagnostics;
+using SiphoEngine;
+using SiphoEngine.Components;
+using SiphoEngine.Core;
+using SiphoEngine.Core.SiphoEngine;
+
+namespace SiphoEngineDemo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Game game = new();
+            TestScene testScene = new TestScene();
+            GameEngine.AddScene(testScene);
+            GameObject gameObject = testScene.CreateGameObject();
+            gameObject.AddComponent<SpriteRenderer>();
+            gameObject.AddComponent<PlayerController>();
+            gameObject.Transform.Position = new(150, 150);
+            game.Run();
+        }
+    }
+
+    public class TestScene : Scene
+    {
+
+        public override void Initialize()
+        {
+            base.Initialize();
         }
     }
 }
